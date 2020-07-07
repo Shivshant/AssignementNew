02dd8f828d66673a901c9d27092fb31064dbb09a
Shivshant <61880399+Shivshant@users.noreply.github.com>
2020-07-07 12:57:22 +0530

    Add files via upload
    Made the necessary changes for filter data.

---
+++ lwc/assignement1/assignement1.js
@@ -0,0 +1,39 @@
+import { LightningElement, wire, track } from 'lwc';
+import getaccounts from '@salesforce/apex/accountController.getaccounts';
+export default class Assignement1 extends LightningElement {
+
+    @track searchkey ;
+    @track numberofrecords;
+    @track myaccounts;
+    @track copiedaccounts;
+    @track error;
+    @track filterDataOperation;
+    @track filtertext;
+
+    handleInput(event){
+       if(event.target.name=='Name'){
+             this.searchkey=event.target.value;
+             console.log(this.searchkey);
+       }else if(event.target.name=='RecordNumber'){
+             this.numberofrecords=event.target.value;
+             console.log(this.numberofrecords);
+       }else if(event.target.name=='SearchFilterText'){
+              // this.filterDataOperation(event.target.value);
+              this.filtertext=event.target.value;
+               
+       }
+    }
+      
+    handleSearchOperation(){
+        
+        getaccounts({searchterm: this.searchkey, noofrecords: this.numberofrecords})
+        .then(result => {this.myaccounts=result;this.copiedaccounts=[...this.myaccounts];})
+        .catch(error => this.error=error);
+         
+    }
+    handleDataFilterOperation(){
+        
+        this.copiedaccounts=this.myaccounts.filter(temp => temp.Name.includes(this.filtertext));
+        
+    }   
+}
